f7304f6a7f7fc395a5dda40ffd8e84bf899fd1b6
Desertdweller1 <donovan.madel@yahoo.com>
2021-07-17 06:28:34 -0700

    tridentDamageBoost and swordDamageBoost implemented

---
+++ src/com/atlasmc/desertdweller/cooking/effects/SwordDamageBoost.java
@@ -0,0 +1,59 @@
+package com.atlasmc.desertdweller.cooking.effects;
+
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
+
+public class SwordDamageBoost extends CustomEffect implements Listener{
+
+	public SwordDamageBoost(int duration, int tier, Player p) {
+		this.player = p;
+		this.duration = duration;
+		this.effect = CookingEffect.SWORDDAMAGEBOOST;
+		this.type = EffectType.BUFF;
+		this.tier = tier;
+		this.name = "expGainOverTime";
+		switch(tier){
+		case 1:
+			this.desc = "Damage done by swords is increased by 5%.";
+			break;
+		case 2:
+			this.desc = "Damage done by swords is increased by 15%.";
+			break;
+		case 3:
+			this.desc = "Damage done by swords is increased by 20%.";
+			break;
+		}
+	}
+	
+	@EventHandler
+	public static void onPlayerDamage(EntityDamageByEntityEvent e) {
+		if(e.getDamager() instanceof Player) {
+			Player p = (Player) e.getDamager();
+			if(!e.getCause().equals(DamageCause.ENTITY_ATTACK) && !e.getCause().equals(DamageCause.ENTITY_SWEEP_ATTACK))
+				return;
+			Material item = p.getInventory().getItemInMainHand().getType();
+			if(!item.equals(Material.DIAMOND_SWORD) && !item.equals(Material.IRON_SWORD) &&
+					!item.equals(Material.STONE_SWORD) && !item.equals(Material.GOLDEN_SWORD) &&
+					!item.equals(Material.WOODEN_SWORD) && !item.equals(Material.NETHERITE_SWORD)) 
+				return;
+			if(EffectsHandler.getEffect(p, CookingEffect.SWORDDAMAGEBOOST) != null) {
+				switch(EffectsHandler.getEffect(p, CookingEffect.SWORDDAMAGEBOOST).tier){
+				case 1:
+					e.setDamage(e.getDamage()*1.05);
+					break;
+				case 2:
+					e.setDamage(e.getDamage()*1.15);
+					break;
+				case 3:
+					e.setDamage(e.getDamage()*1.20);
+					break;
+				}
+			}
+		}
+	}
+
+}
--- src/com/atlasmc/desertdweller/cooking/effects/TridentDamageBoost.java
+++ src/com/atlasmc/desertdweller/cooking/effects/TridentDamageBoost.java
@@ -1,33 +1,33 @@
 package com.atlasmc.desertdweller.cooking.effects;
 
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
 
-public class MeleeDamageBoost extends CustomEffect implements Listener{
+public class TridentDamageBoost extends CustomEffect implements Listener{
 
-	public MeleeDamageBoost(int duration, int tier, Player p) {
+	public TridentDamageBoost(int duration, int tier, Player p) {
 		this.player = p;
 		this.duration = duration;
-		this.effect = CookingEffect.MELEEDAMAGEBOOST;
+		this.effect = CookingEffect.TRIDENTDAMAGEBOOST;
 		this.type = EffectType.BUFF;
 		this.tier = tier;
-		this.name = "Champion's Might";
+		this.name = "tridentDamageBoost";
 		switch(tier){
 		case 1:
-			this.desc = "General melee damage increased by 10%.";
+			this.desc = "Damage done by tridents is increased by 5%.";
 			break;
 		case 2:
-			this.desc = "General melee damage increased by 15%.";
+			this.desc = "Damage done by tridents is increased by 15%.";
 			break;
 		case 3:
-			this.desc = "General melee damage increased by 20%.";
+			this.desc = "Damage done by tridents is increased by 20%.";
 			break;
 		}
 	}
-	
 	
 	@EventHandler
 	public static void onPlayerDamage(EntityDamageByEntityEvent e) {
@@ -35,10 +35,12 @@ public class MeleeDamageBoost extends CustomEffect implements Listener{
 			Player p = (Player) e.getDamager();
 			if(!e.getCause().equals(DamageCause.ENTITY_ATTACK) && !e.getCause().equals(DamageCause.ENTITY_SWEEP_ATTACK))
 				return;
-			if(EffectsHandler.getEffect(p, CookingEffect.MELEEDAMAGEBOOST) != null) {
-				switch(EffectsHandler.getEffect(p, CookingEffect.MELEEDAMAGEBOOST).tier){
+			if(!p.getInventory().getItemInMainHand().getType().equals(Material.TRIDENT)) 
+				return;
+			if(EffectsHandler.getEffect(p, CookingEffect.TRIDENTDAMAGEBOOST) != null) {
+				switch(EffectsHandler.getEffect(p, CookingEffect.TRIDENTDAMAGEBOOST).tier){
 				case 1:
-					e.setDamage(e.getDamage()*1.10);
+					e.setDamage(e.getDamage()*1.05);
 					break;
 				case 2:
 					e.setDamage(e.getDamage()*1.15);
